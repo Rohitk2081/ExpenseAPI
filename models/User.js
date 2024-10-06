494e492ed33742ebf2ac99ac4661d01f94b87f03
rohit <rohitkumar00n@gmail.com>
2024-10-06 17:12:52 +0530

    Initial commit

--- models/User.js
+++ models/User.js
@@ -4,4 +4,5 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 }, { timestamps: true });
+
 module.exports = mongoose.model('User', UserSchema);
